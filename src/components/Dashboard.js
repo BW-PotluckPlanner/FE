157b7333c11320f953882d7a6d00a2a83d5c6fe1
Jacob Morris <jacobthomasmorris@gmail.com>
2021-01-06 15:53:13 -0700

    finished DisplayDate and Edit Date Components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,18 @@
 import Event from './Event';
 import CreateNewEvent from './CreateNewEvent';
 import {useEffect, useState} from 'react';
+import axiosWithAuth from '../api/axiosWithAuth';
 
 
 function Dashboard() {
   const [events, setEvents] = useState([]);
+  const id = localStorage.getItem("userId");
+  console.log('userId', id);
   const getData = () =>{
-    //when endpoint is set up the data for the user's events will be pulled from database, for now I am setting it statically. 
-    setEvents([{title: 'Family Dinner', date: '11/25', time: '12:00pm', rsvp: false}, {title: 'Friends Giving', date: '11/25', time: '12:00pm', rsvp: true}]);
+    axiosWithAuth().get(`api/potluck/${id}/mypotlucks`).then(res =>{
+      console.log(res.data);
+      setEvents(res.data);
+    })
   }
 
   useEffect(getData, []);
