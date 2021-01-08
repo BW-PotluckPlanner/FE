89e637509e380240af23a51bf2a0a3e2b28aa751
Jacob Morris <jacobthomasmorris@gmail.com>
2021-01-08 10:54:57 -0700

    Finished Jacob Components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EditDate from './components/EditDate'
 import Event from './components/Event'
 import {useEffect, useState} from 'react';
 import Dashboard from './components/Dashboard';
-import { Route } from "react-router-dom";
+import { Route, Switch, Link, Redirect } from "react-router-dom";
 import Login from './components/Login';
 import Register from './components/Register';
 import Header from './components/Header'
@@ -13,13 +13,26 @@ import Header from './components/Header'
 
 function App() {
 
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <Header/>
         <hr></hr>
       </header>
-        <Route exact path="/" component={Dashboard}/>
+        <PrivateRoute exact path="/" component={Dashboard}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
     </div>
