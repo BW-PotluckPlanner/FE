a5f6a480735b4f7df6b86175111e69d0395c9824
Ryan Clark <rbclark244@gmail.com>
2021-01-07 21:20:21 -0500

    strike through added to css

--- src/components/FoodListItem.js
+++ src/components/FoodListItem.js
@@ -1,13 +1,3 @@
-//1. Create a new list item component. FoodListItem.js
-//2. Make a get request to an endpoint to see if someone clicked
-//2a. getPersonBringing(id) -> either empty or the name of the person who clicked (put into state var)
-//2b. If empty do nothing
-//2c. If not empty then you can attach a css class to the link element to strike it through, you can also create a new <p> component with the name of the person
-//3. Just return a link element
-//4. Add a click event to the list item
-//4a. Post to the new API -> responds with name
-//4b. Take that name and put it into the state variable
-
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../api/axiosWithAuth";
 
